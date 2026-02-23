2def9cb36a29ba665531f91105cec6ec843757c4
Orhon Can Dağtekin <66990865+orhoncan@users.noreply.github.com>
2026-02-23 13:03:13 +0300

    chore: clean repo and ignore local artifacts

--- orhons-zotero-dashboard/README.md
+++
@@ -1,5 +0,0 @@
-# Orhon'un Zotero Paneli / Orhon's Zotero Dashboard
-
-- Türkçe rehber: [README_TR.md](README_TR.md)
-- English guide: [README_EN.md](README_EN.md)
-
--- orhons-zotero-dashboard/assets/js/base.js
+++
@@ -1,8 +0,0 @@
-(function (window) {
-  const ns = (window.ZoteroDashboard = window.ZoteroDashboard || {});
-  ns.API = "";
-  ns.mixins = ns.mixins || [];
-  ns.registerMixin = function registerMixin(factory) {
-    ns.mixins.push(factory);
-  };
-})(window);
